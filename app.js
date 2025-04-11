0e016e1d679f2a665ebfa30446323b7b56fdb5bb
funfloo <florian.mazzilli@esme.fr>
2025-04-11 17:42:52 +0200

    last commit

--- app.js
+++ app.js
@@ -1,7 +1,312 @@
 const contractAddress = '0xa880168E04D16fE368Dc05578394d5eb5b321b65';
 const abi = [
-    // Add your contract ABI here
-];
+    {
+     "inputs": [
+      {
+       "internalType": "address",
+       "name": "participant",
+       "type": "address"
+      }
+     ],
+     "name": "addToWhitelist",
+     "outputs": [],
+     "stateMutability": "nonpayable",
+     "type": "function"
+    },
+    {
+     "inputs": [
+      {
+       "internalType": "string",
+       "name": "manufacturer",
+       "type": "string"
+      },
+      {
+       "internalType": "uint256",
+       "name": "lotNumber",
+       "type": "uint256"
+      },
+      {
+       "internalType": "string",
+       "name": "productName",
+       "type": "string"
+      },
+      {
+       "internalType": "string",
+       "name": "lotId",
+       "type": "string"
+      },
+      {
+       "internalType": "uint256",
+       "name": "totalProducts",
+       "type": "uint256"
+      },
+      {
+       "internalType": "uint256",
+       "name": "purchaseDate",
+       "type": "uint256"
+      }
+     ],
+     "name": "createProduct",
+     "outputs": [
+      {
+       "internalType": "uint256",
+       "name": "",
+       "type": "uint256"
+      }
+     ],
+     "stateMutability": "nonpayable",
+     "type": "function"
+    },
+    {
+     "inputs": [
+      {
+       "internalType": "address",
+       "name": "participant",
+       "type": "address"
+      }
+     ],
+     "name": "removeFromWhitelist",
+     "outputs": [],
+     "stateMutability": "nonpayable",
+     "type": "function"
+    },
+    {
+     "inputs": [],
+     "stateMutability": "nonpayable",
+     "type": "constructor"
+    },
+    {
+     "anonymous": false,
+     "inputs": [
+      {
+       "indexed": false,
+       "internalType": "uint256",
+       "name": "productId",
+       "type": "uint256"
+      },
+      {
+       "indexed": true,
+       "internalType": "address",
+       "name": "from",
+       "type": "address"
+      },
+      {
+       "indexed": true,
+       "internalType": "address",
+       "name": "to",
+       "type": "address"
+      }
+     ],
+     "name": "OwnershipTransferred",
+     "type": "event"
+    },
+    {
+     "anonymous": false,
+     "inputs": [
+      {
+       "indexed": false,
+       "internalType": "uint256",
+       "name": "productId",
+       "type": "uint256"
+      },
+      {
+       "indexed": true,
+       "internalType": "address",
+       "name": "owner",
+       "type": "address"
+      }
+     ],
+     "name": "ProductCreated",
+     "type": "event"
+    },
+    {
+     "inputs": [
+      {
+       "internalType": "uint256",
+       "name": "productId",
+       "type": "uint256"
+      },
+      {
+       "internalType": "address",
+       "name": "newOwner",
+       "type": "address"
+      }
+     ],
+     "name": "transferOwnership",
+     "outputs": [],
+     "stateMutability": "nonpayable",
+     "type": "function"
+    },
+    {
+     "anonymous": false,
+     "inputs": [
+      {
+       "indexed": true,
+       "internalType": "address",
+       "name": "participant",
+       "type": "address"
+      },
+      {
+       "indexed": false,
+       "internalType": "bool",
+       "name": "isAdded",
+       "type": "bool"
+      }
+     ],
+     "name": "WhitelistUpdated",
+     "type": "event"
+    },
+    {
+     "inputs": [
+      {
+       "internalType": "uint256",
+       "name": "productId",
+       "type": "uint256"
+      }
+     ],
+     "name": "getProduct",
+     "outputs": [
+      {
+       "components": [
+        {
+         "internalType": "string",
+         "name": "manufacturer",
+         "type": "string"
+        },
+        {
+         "internalType": "uint256",
+         "name": "lotNumber",
+         "type": "uint256"
+        },
+        {
+         "internalType": "string",
+         "name": "productName",
+         "type": "string"
+        },
+        {
+         "internalType": "string",
+         "name": "lotId",
+         "type": "string"
+        },
+        {
+         "internalType": "uint256",
+         "name": "totalProducts",
+         "type": "uint256"
+        },
+        {
+         "internalType": "address",
+         "name": "currentOwner",
+         "type": "address"
+        },
+        {
+         "internalType": "uint256",
+         "name": "purchaseDate",
+         "type": "uint256"
+        }
+       ],
+       "internalType": "struct ProductTraceability.Product",
+       "name": "",
+       "type": "tuple"
+      }
+     ],
+     "stateMutability": "view",
+     "type": "function"
+    },
+    {
+     "inputs": [],
+     "name": "owner",
+     "outputs": [
+      {
+       "internalType": "address",
+       "name": "",
+       "type": "address"
+      }
+     ],
+     "stateMutability": "view",
+     "type": "function"
+    },
+    {
+     "inputs": [],
+     "name": "productCount",
+     "outputs": [
+      {
+       "internalType": "uint256",
+       "name": "",
+       "type": "uint256"
+      }
+     ],
+     "stateMutability": "view",
+     "type": "function"
+    },
+    {
+     "inputs": [
+      {
+       "internalType": "uint256",
+       "name": "",
+       "type": "uint256"
+      }
+     ],
+     "name": "products",
+     "outputs": [
+      {
+       "internalType": "string",
+       "name": "manufacturer",
+       "type": "string"
+      },
+      {
+       "internalType": "uint256",
+       "name": "lotNumber",
+       "type": "uint256"
+      },
+      {
+       "internalType": "string",
+       "name": "productName",
+       "type": "string"
+      },
+      {
+       "internalType": "string",
+       "name": "lotId",
+       "type": "string"
+      },
+      {
+       "internalType": "uint256",
+       "name": "totalProducts",
+       "type": "uint256"
+      },
+      {
+       "internalType": "address",
+       "name": "currentOwner",
+       "type": "address"
+      },
+      {
+       "internalType": "uint256",
+       "name": "purchaseDate",
+       "type": "uint256"
+      }
+     ],
+     "stateMutability": "view",
+     "type": "function"
+    },
+    {
+     "inputs": [
+      {
+       "internalType": "address",
+       "name": "",
+       "type": "address"
+      }
+     ],
+     "name": "whitelist",
+     "outputs": [
+      {
+       "internalType": "bool",
+       "name": "",
+       "type": "bool"
+      }
+     ],
+     "stateMutability": "view",
+     "type": "function"
+    }
+   ];
 
 let web3;
 let contract;
